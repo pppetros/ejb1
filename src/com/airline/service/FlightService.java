82f5dfaa0c86cc20a2e4c1b156408ecb799e903d
pppetros <pppetros@gmail.com>
2018-05-23 18:30:17 +0300

    ejb1

---
+++ src/com/airline/service/FlightService.java
@@ -0,0 +1,20 @@
+package com.airline.service;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class FlightService
+ */
+@Stateless
+@LocalBean
+public class FlightService {
+
+    /**
+     * Default constructor. 
+     */
+    public FlightService() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
